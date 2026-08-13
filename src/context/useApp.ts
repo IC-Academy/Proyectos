c372dbdd9f20eeb64fa8b1909d7c3ee420cd1fec
IC-ACADEMY <jmejia@intercon.com.mx>
2026-08-13 13:01:46 -0600

    v1

--- src/context/useApp.ts
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { AppContext, type AppContextValue } from "./AppContextBase";
-
-export function useApp(): AppContextValue {
-  const ctx = useContext(AppContext);
-  if (!ctx) throw new Error("useApp debe usarse dentro de <AppProvider>");
-  return ctx;
-}
